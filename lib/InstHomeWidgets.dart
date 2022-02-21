e23fdfd4893c88c21bb8c9051a58333a4d498c50
ZiyadNass <82228302+ZiyadNass@users.noreply.github.com>
2022-02-21 23:51:33 +0300

    fourd try

--- lib/InstHomeWidgets.dart
+++ lib/InstHomeWidgets.dart
@@ -125,7 +125,7 @@ class InatMainBtn extends StatelessWidget {
                 size: 32,
               ),
               Text(
-                'Fuck u',
+                'Fuck omar <#',
                 style: TextStyle(fontSize: 10, color: Colors.white),
               ),
             ],
