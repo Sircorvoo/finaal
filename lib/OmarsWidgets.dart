e0c01d333f81f5012b797550bbab291412d5a7d4
Omar Abdullah Al-Harf <74682890+Sircorvoo@users.noreply.github.com>
2022-02-22 19:34:40 +0300

    1-Login vis 1.00 2-New Clean Reg Page

--- lib/OmarsWidgets.dart
+++ lib/OmarsWidgets.dart
@@ -8,7 +8,7 @@ import 'InstaAddCSWidgets.dart';
 class CSfieldEmail extends StatelessWidget {
   final String _title;
   final String _hint;
-  const CSfieldEmail(this._title,this._hint);
+  const CSfieldEmail(this._title, this._hint);
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +19,15 @@ class CSfieldEmail extends StatelessWidget {
       //mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-
         Padding(
           padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-          child: Container(child: Text(_title,
-            style: TextStyle(
-                color: Colors.black45
-            ),),),
+          child: Container(
+            child: Text(
+              _title,
+              style: TextStyle(color: Colors.black45),
+            ),
+          ),
         ),
-
         Container(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(15, 2, 15, 8),
@@ -36,10 +36,8 @@ class CSfieldEmail extends StatelessWidget {
                   border: Border(bottom: BorderSide(color: Colors.black45))),
               child: TextFormField(
                 keyboardType: TextInputType.emailAddress,
-
-
                 decoration: InputDecoration(
-                  //labelText:_Text ,
+                    //labelText:_Text ,
                     fillColor: Color(0xFFECECEA),
                     filled: true,
                     hintText: _hint,
@@ -50,5 +48,18 @@ class CSfieldEmail extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class CSButton extends StatelessWidget {
+  const CSButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: ElevatedButton(
+      child: Text("Login"),
+      onPressed: () {},
+    ));
   }
 }
