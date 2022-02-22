9efc4dc492ad1b2ebab257b5711f0db157a95888
ZiyadNass <82228302+ZiyadNass@users.noreply.github.com>
2022-02-23 02:21:02 +0300

    Password Fixed

--- lib/OmarsWidgets.dart
+++ lib/OmarsWidgets.dart
@@ -59,8 +59,8 @@ class CSButton extends StatelessWidget {
     final LengthSized = MediaQuery.of(context).size.height;
     final WidthSized = MediaQuery.of(context).size.width;
     return Container(
-        child: ElevatedButton(
-        child: Text(_text),
+        child: OutlinedButton(
+      child: Text(_text),
       onPressed: () {},
     ));
   }
@@ -112,11 +112,17 @@ class CSPhoneFiled extends StatelessWidget {
   }
 }
 
-class CSPassword extends StatelessWidget {
+class CSPassword extends StatefulWidget {
   final String _title;
   final String _hint;
   const CSPassword(this._title, this._hint);
 
+  @override
+  State<CSPassword> createState() => _CSPasswordState();
+}
+
+class _CSPasswordState extends State<CSPassword> {
+  bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
     final LengthSized = MediaQuery.of(context).size.height;
@@ -130,7 +136,7 @@ class CSPassword extends StatelessWidget {
           padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
           child: Container(
             child: Text(
-              _title,
+              widget._title,
               style: TextStyle(color: Colors.black45),
             ),
           ),
@@ -142,13 +148,22 @@ class CSPassword extends StatelessWidget {
               decoration: BoxDecoration(
                   border: Border(bottom: BorderSide(color: Colors.black45))),
               child: TextFormField(
-                keyboardType: TextInputType.text,
-                obscureText: true,
+                obscureText: _isObscure,
                 decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                          _isObscure ? Icons.visibility : Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                    ),
+
                     //labelText:_Text ,
                     fillColor: Color(0xFFECECEA),
                     filled: true,
-                    hintText: _hint,
+                    hintText: widget._hint,
                     border: OutlineInputBorder(borderSide: BorderSide.none)),
               ),
             ),
@@ -158,3 +173,4 @@ class CSPassword extends StatelessWidget {
     );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'InstAddCS.dart';
 import 'InstHomePage.dart';
 import 'InstLoginPage.dart';
 import 'InstRegisterPage.dart';
+import 'OmarsWidgets.dart';
+
 void main() => runApp(MaterialApp(
   home: InstRegisterPage(),
 ));
